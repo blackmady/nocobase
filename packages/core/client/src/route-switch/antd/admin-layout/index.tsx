cc26a9773a100f8743006c640a447bf21ad88273
sheldon66 <sheldon_66@163.com>
2024-08-16 11:53:49 +0800

    feat: temp

--- packages/core/client/src/route-switch/antd/admin-layout/index.tsx
+++ packages/core/client/src/route-switch/antd/admin-layout/index.tsx
@@ -33,6 +33,7 @@ import {
   useSystemSettings,
   useToken,
 } from '../../../';
+import { InApp } from '../../../in-app';
 import { useLocationNoUpdate, useNavigateNoUpdate } from '../../../application/CustomRouterContextProvider';
 import { Plugin } from '../../../application/Plugin';
 import { useAppSpin } from '../../../application/hooks/useAppSpin';
@@ -487,6 +488,7 @@ export const InternalAdminLayout = () => {
               <Divider type="vertical" />
             </ConfigProvider>
             <Help />
+            <InApp />
             <CurrentUser />
           </div>
         </div>
